0db7ecd4e2971477e2f0518dc1c3cce9a2f24eb8
benjamin0906 <benjamin0906@gmail.com>
2021-09-03 18:44:17 +0100

    Handling of state querry has been implemented.

--- CommThread/CommThread.c
+++ CommThread/CommThread.c
@@ -1,20 +1,12 @@
-#define _POSIX_C_SOURCE 200809L
 #include "CommThread_Types.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <signal.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "Utilities.h"
-
-pthread_t Thread;
-pthread_mutex_t Mutex;
+#include "Shared.h"
 
 void *CommThreadFunc(void *var);
-
-extern uint32 cntr;
 
 void *CommThreadFunc(void *var)
 {
@@ -23,12 +15,11 @@ void *CommThreadFunc(void *var)
     Server.sin_family = AF_INET;
     Server.sin_port = Swap16(9999);
     Server.sin_addr.s_addr = Swap32(0xC0A80002);
-    int s = 0;
+    int32 s = 0;
 
     s = socket(AF_INET, SOCK_DGRAM, 0);
     if(s >= 0)
     {
-        char buf[32];
         if(bind(s, (struct sockaddr *)&Server, sizeof(Server)) >= 0)
         {
             int Namelen = sizeof(Server);
@@ -36,26 +27,54 @@ void *CommThreadFunc(void *var)
             printf("Port assigned is %d\n", ntohs(Server.sin_port));
 
             uint32 ClientAddrSize = sizeof(client);
-            while(1){
-            if(recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&client, &ClientAddrSize) >= 0)
+            uint8 buff[8];
+            uint8 SeqCntr = 1;
+            while(1)
             {
-                printf("received: %s\n", buf);
+                uint32 RxLen = recvfrom(s, buff, sizeof(buff), 0, (struct sockaddr *)&client, &ClientAddrSize);
+                if(RxLen > 0)
+                {
+                    for(int i=0; i < RxLen; i++) printf("%x ", buff[i]);
+                    printf("\n");
+
+                    /* Checking the sequence counter */
+                    if(buff[1] != SeqCntr)
+                    {
+                        printf("SeqCntr is wrong\n");
+                        SeqCntr = buff[1];
+                    }
+                    switch(buff[0])
+                    {
+                    case Msg_StateReq:
+                    {
+                        dtStateMsg Msg;
+                        Msg.Fields.MsgType = Msg_State;
+                        Msg.Fields.SeqCntr = SeqCntr;
+                        Msg.Fields.Engaged = Shared_GetEngaged();
+                        Msg.Fields.CurrentSpeed = Shared_GetCurrSpeed();
+                        Msg.Fields.CurrentSWA = Shared_GetCurrSWA();
+                        sendto(s, &Msg.Byte, sizeof(dtStateMsg), 0, (struct sockaddr *)&client, ClientAddrSize);
+                        break;
+                    }
+                    case Msg_SetEngage:
+                        
+                        break;
+                    case Msg_SetReferences:
+                        
+                        break;
+                    default:
+                        break;
+                    }
+
+                    /* Setting the SeqCntr to the next one */
+                    SeqCntr++;
+                    if(SeqCntr == 0) SeqCntr++;
+                }
             }
-}
         }
         else printf("Problem with binding\n");
     }
     else printf("socket creating error: %d\n", s);
 
-
-
-/*// while(1)
-pthread_mutex_lock(Mutex);
-    for(int i=0; i<5; i++)
-    {
-        for(int j = 0; j< 0x7ffffff; j++);
-        printf("Comm thread is me, %d\n", cntr);
-    }
-pthread_mutex_unlock(Mutex);*/
     return 0;
 }
--- CommThread/CommThread_Types.h
+++ CommThread/CommThread_Types.h
@@ -1,1 +1,22 @@
 #include "types.h"
+
+enum eMsgTypes
+{
+    Msg_StateReq,
+    Msg_State,
+    Msg_SetEngage,
+    Msg_SetReferences
+};
+
+typedef union
+{
+    struct
+    {
+        uint8 MsgType;
+        uint8 SeqCntr;
+        uint8 Engaged;
+        int16 CurrentSpeed;
+        int16 CurrentSWA;
+    } Fields;
+    uint8 Byte;
+} dtStateMsg;
